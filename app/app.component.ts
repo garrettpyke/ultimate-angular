28f4e702659a4b7b4f6d9704e2b258cb8504259c
Garrett <garrettpyke@msn.com>
2022-11-17 16:42:58 -0700

    Set up one-way & two-way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,23 +6,27 @@ import { Component } from "@angular/core";
   template: `
       <div class="app">
         <h2>{{ title + '!' }}</h2>
-        <h2 [innerHTML]="title"></h2>
-        <img [src]="logo">
-        <div>
-          {{ numberOne + numberTwo }}
-        </div>
-        <div>
-          {{ isHappy ? ':)' : ':(' }}
-      </div>  
+        <button (click)="handleClick()">change name</button>
+        <input type="text"
+              [ngModel]="name"
+              (ngModelChange)="handleChange($event)">
+        <input type="text"
+              [(ngModel)]="name">
+        <div>{{ name }}</div>
+      </div>
     `
 })
 export class AppComponent {
   title: string;
-  logo: string = 'img/logo.svg';
+  name: string = 'Garrett';
   isHappy: boolean = true;
-  numberOne: number = 1;
-  numberTwo: number = 2;
   constructor() {
     this.title = 'Ultimate Angular';
+  }
+  handleClick() {
+    this.name = 'Pyke';
+  }
+  handleChange(value: string) {
+    this.name = value;
   }
 }
